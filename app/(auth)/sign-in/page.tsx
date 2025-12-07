710f03dadd62167f95e7b33d930aeb01d15ae1b4
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-12-07 21:38:04 +0300

    add auth form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,16 @@
+'use client';
+import { AuthForm } from '@/components/forms/AuthForm';
+import { SignInSchema } from '@/lib/validations';
+
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{ email: '', password: '' }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignInPage;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+
+import { AuthForm } from '@/components/forms/AuthForm';
+import { SignUpSchema } from '@/lib/validations';
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: '', password: '', name: '', username: '' }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignUpPage;
