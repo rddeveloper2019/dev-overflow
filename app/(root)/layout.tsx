c53af36913af24386ab316a8d12068535e4c7573
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-12-08 22:29:12 +0300

    right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import { LeftSidebar } from '@/components/navigation/LeftSidebar';
 import Navbar from '@/components/navigation/navbar';
+import { RightSidebar } from '@/components/navigation/RightSidebar';
 import { ReactNode } from 'react';
 
 export default function RootLayout({
@@ -17,6 +18,7 @@ export default function RootLayout({
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+        <RightSidebar />
       </div>
     </main>
   );
