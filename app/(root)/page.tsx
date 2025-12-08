f2fec2035e61d7aca6164e45cbd84226c18075e4
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-12-08 22:54:40 +0300

    init main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,32 @@
 import { auth, signOut } from '@/auth';
 import { Button } from '@/components/ui/button';
 import ROUTES from '@/constants/routes';
+import Link from 'next/link';
 
 const Home = async () => {
   const session = await auth();
   console.log(session);
   return (
-    <div>
-      <h1 className="h1-bold ">Welcome to the world of Next.js</h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of Next.js
-      </h1>
-      {/* <form
-        action={async () => {
-          'use server';
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form> */}
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
+    </>
   );
 };
 
