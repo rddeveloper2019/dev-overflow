9c925d55fe739af7565e158907aee0f2461a52fb
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-12-08 21:51:28 +0300

    left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import { LeftSidebar } from '@/components/navigation/LeftSidebar';
 import Navbar from '@/components/navigation/navbar';
 import { ReactNode } from 'react';
 
@@ -7,9 +8,16 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <main>
+    <main className="background-light850_dark100 realtive">
       <Navbar />
-      {children}
+
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
     </main>
   );
 }
